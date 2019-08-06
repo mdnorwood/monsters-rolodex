202eb251518df954eb3f2c0d5c76d7086a5e793d
Marcus Norwood <marcusnorwood@MacBook-Pro-2.local>
2019-08-06 10:59:20 -0600

    Updates

---
+++ precache-manifest.51d4cfceb69c31dea3b2ce17a3c21227.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6670656383f124e9be5ba5902b88892",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5b50dd3940930c9d68e3",
+    "url": "/monsters-rolodex/static/css/main.5f2d5b2b.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "5b50dd3940930c9d68e3",
+    "url": "/monsters-rolodex/static/js/main.89fdbb63.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
